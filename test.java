3ec3bba58b04c168461609a4ccfe25efb9548353
Rajoshi Basu <34625067+R1A2J@users.noreply.github.com>
2023-02-12 02:32:46 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,3 @@
-package usacocomp;
 import java.io.*;
 import java.util.*;
 /*
@@ -8,13 +7,9 @@ TASK: test
 */
 class test {
     public static void main (String [] args) throws IOException {
-        // Use BufferedReader rather than RandomAccessFile; it's much faster
         BufferedReader f = new BufferedReader(new FileReader("test.in"));
-        // input file name goes above
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("test.out")));
-        // Use StringTokenizer vs. readLine/split -- lots faster
         StringTokenizer st = new StringTokenizer(f.readLine());
-        // Get line, break into tokens
         int i1 = Integer.parseInt(st.nextToken());    // first integer
         int i2 = Integer.parseInt(st.nextToken());    // second integer
         out.println(i1+i2);                           // output result
