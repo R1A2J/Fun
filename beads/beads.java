3aa0d94752c5f31ae5c67052382781df176e3a5b
Rajoshi Basu <34625067+R1A2J@users.noreply.github.com>
2023-02-12 20:12:28 -0500

    Update beads.java

--- beads/beads.java
+++ beads/beads.java
@@ -9,58 +9,48 @@ public class beads {
     public static void main(String[] args) throws IOException {
         BufferedReader f = new BufferedReader(new FileReader("beads.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("beads.out")));
-        int n = Integer.parseInt(f.readLine());
-        char ch[] = f.readLine().toCharArray();
-        int count = 0;
-        myBreakLabel:
-        for (int a = 0; a < n; a++) {
-            int i = a;
-            int j = 0;
-            List<Integer> list = new ArrayList<>();
-            while (ch[i] == 'w') {
-                list.add(i);
-                if (i == (n - 1)) {
-                    i = 0;
-                } else {
-                    i++;
+        for (int i = 0; i < 9; i++) {
+            int n = Integer.parseInt(f.readLine());
+            char ch[] = f.readLine().toCharArray();
+            int count = 0;
+            myBreakLabel:
+            for (int a = 0; a < n; a++) {
+                int i = a;
+                int j = 0;
+                List<Integer> list = new ArrayList<>();
+                while (ch[i] == 'w') {
+                    list.add(i);
+                    if (i == (n - 1)) {
+                        i = 0;
+                    } else {
+                        i++;
+                    }
+                    j++;
+                    if (j == n) {
+                        count = n;
+                        break myBreakLabel;
+                    }
                 }
-                j++;
-                if (j == n) {
-                    count = n;
-                    break myBreakLabel;
+                char abc = ch[i];
+                while ((ch[i] == abc) || (ch[i] == 'w')) {
+                    list.add(i);
+                    if (i == (n - 1)) {
+                        i = 0;
+                    } else {
+                        i++;
+                    }
+                    j++;
+                    if (j == n) {
+                        count = n;
+                        break myBreakLabel;
+                    }
                 }
-            }
-            char abc = ch[i];
-            while ((ch[i] == abc) || (ch[i] == 'w')) {
-                list.add(i);
-                if (i == (n - 1)) {
-                    i = 0;
-                } else {
-                    i++;
-                }
-                j++;
-                if (j == n) {
-                    count = n;
-                    break myBreakLabel;
-                }
-            }
-            if (a == 0) {
-                i = n - 1;
-            } else {
-                i = a - 1;
-            }
-            while (ch[i] == 'w') {
-                list.add(i);
-                if (i == 0) {
+                if (a == 0) {
                     i = n - 1;
                 } else {
-                    i--;
+                    i = a - 1;
                 }
-                j++;
-            }
-            abc = ch[i];
-            while ((ch[i] == abc) || (ch[i] == 'w')) {
-                if (!list.contains(i)) {
+                while (ch[i] == 'w') {
                     list.add(i);
                     if (i == 0) {
                         i = n - 1;
@@ -68,16 +58,28 @@ public class beads {
                         i--;
                     }
                     j++;
-                } else {
-                    count = n;
-                    break myBreakLabel;
+                }
+                abc = ch[i];
+                while ((ch[i] == abc) || (ch[i] == 'w')) {
+                    if (!list.contains(i)) {
+                        list.add(i);
+                        if (i == 0) {
+                            i = n - 1;
+                        } else {
+                            i--;
+                        }
+                        j++;
+                    } else {
+                        count = n;
+                        break myBreakLabel;
+                    }
+                }
+                if (count < j) {
+                    count = j;
                 }
             }
-            if (count < j) {
-                count = j;
-            }
+            out.println(count);
         }
-        out.println(count);
         out.close();
     }
 }
