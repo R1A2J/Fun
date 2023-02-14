0f298749af7fd74ce28777be4345d72ac663871e
Rajoshi Basu <34625067+R1A2J@users.noreply.github.com>
2023-02-13 20:49:15 -0500

    Update gift1.java

--- gift1/gift1.java
+++ gift1/gift1.java
@@ -9,51 +9,50 @@ public class gift1 {
     public static void main(String[] args) throws IOException {
         BufferedReader f = new BufferedReader(new FileReader("gift1.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("gift1.out")));
-        int a = Integer.parseInt(f.readLine());
-        int price[] = new int[a];
-        String names[] = new String[a];
-        int i = 0;
-        while (i < a) {
-            names[i] = f.readLine();
-            i++;
-        }
-        for (int kk = 0; kk < a; kk++) {
-            String j = f.readLine();
-            i = 0;
-            while (!(j.equals(names[i]))) {
+        for (int abcd = 0; abcd < 9; abcd++) {
+            int a = Integer.parseInt(f.readLine());
+            int price[] = new int[a];
+            String names[] = new String[a];
+            int i = 0;
+            while (i < a) {
+                names[i] = f.readLine();
                 i++;
             }
-            StringTokenizer k = new StringTokenizer(f.readLine());
-            int aaa = Integer.parseInt(k.nextToken());
-            int b = Integer.parseInt(k.nextToken());
-            if ((aaa == 0) || (b == 0)) {
-                int abc = 0;
-                while (abc < b) {
-                    f.readLine();
-                    abc++;
+            for (int kk = 0; kk < a; kk++) {
+                String j = f.readLine();
+                i = 0;
+                while (!(j.equals(names[i]))) {
+                    i++;
                 }
-                continue;
-            }
-            price[i] += ((aaa % b) - aaa);
-            for (i = 0; i < b; i++) {
-                String jj = f.readLine();
-                int iii = 0;
-                while (!(jj.equals(names[iii]))) {
-                    if (j.equals(names[iii])) {
-                        iii++;
-                        continue;
+                StringTokenizer k = new StringTokenizer(f.readLine());
+                int aaa = Integer.parseInt(k.nextToken());
+                int b = Integer.parseInt(k.nextToken());
+                if ((aaa == 0) || (b == 0)) {
+                    int abc = 0;
+                    while (abc < b) {
+                        f.readLine();
+                        abc++;
                     }
-                    iii++;
+                    continue;
                 }
-                price[iii] += (aaa/b);
+                price[i] += ((aaa % b) - aaa);
+                for (i = 0; i < b; i++) {
+                    String jj = f.readLine();
+                    int iii = 0;
+                    while (!(jj.equals(names[iii]))) {
+                        if (j.equals(names[iii])) {
+                            iii++;
+                            continue;
+                        }
+                        iii++;
+                    }
+                    price[iii] += (aaa/b);
+                }
             }
-        }
-        for (i = 0; i < a; i++) {
-            out.println(names[i] + " " + price[i]);
+            for (i = 0; i < a; i++) {
+                out.println(names[i] + " " + price[i]);
+            }
         }
         out.close();
     }
 }
-/*a = 10, i = 0, names = {mitnik, poulsen, tanner, stallman, ritchie, baran, spafford, farmer, venema, linus}, kk = 1, j = "mitnik", aaa = 300, b = 3,
-price = {-300, 100, 100, 0, 0, 100, 0, 0, 0, 0}, jj = "poulsen", iii = 1
-*/
